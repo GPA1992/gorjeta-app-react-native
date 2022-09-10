2ec0554530790f51e5543aff0033ef8d0dbad9b9
Gabriel <gabrielproencaaraujo@gmail.com>
2022-09-10 20:05:24 -0300

    Iniciando CSS

--- gorjeta-app/src/screens/home/Main.jsx
+++ gorjeta-app/src/screens/home/Main.jsx
@@ -1,10 +1,12 @@
 import {
-  View, Button, Text,
+  Button,
 } from 'react-native';
 import React, { useState } from 'react';
 import CurrencyInput from 'react-native-currency-input';
 import {
-  Container, Title, InputContainer, Input,
+  Title, InputContainer, Input, Header,
+  Body, PercentContainer, Percent, Form,
+  Paragraph, Div,
 } from './Style';
 
 export default function Main() {
@@ -22,49 +24,47 @@ export default function Main() {
   };
 
   return (
-    <View>
-      <Container>
-
+    <Body>
+      <Header>
         <Title>Gorjetas</Title>
-        <View>
-
-          <Text>Digite o valor total do pedido</Text>
-
-          <InputContainer>
-            <Text>R$</Text>
-            <Input>
-              <CurrencyInput
-                placeholder="0,00"
-                value={requestValue}
-                onChangeValue={setRequestValue}
-                prefix=""
-                delimiter="."
-                separator=","
-                precision={2}
-                style={{
-                  TitleAlign: 'right',
-                  paddingRight: 5,
-                }}
-              />
-            </Input>
-          </InputContainer>
-
-          <Text>Quanto quer dar de gorjeta?</Text>
-          <Button
+      </Header>
+      <Form>
+        <Paragraph>Digite o valor total do pedido</Paragraph>
+        <InputContainer>
+          <Paragraph>R$</Paragraph>
+          <Input>
+            <CurrencyInput
+              placeholder="0,00"
+              value={requestValue}
+              onChangeValue={setRequestValue}
+              prefix=""
+              delimiter="."
+              separator=","
+              precision={2}
+              style={{
+                textAlign: 'right',
+                paddingRight: 5,
+              }}
+            />
+          </Input>
+        </InputContainer>
+        <Paragraph>Quanto quer dar de gorjeta?</Paragraph>
+        <PercentContainer>
+          <Percent
             title="5%"
             onPress={() => getTipPercent(5)}
           />
-          <Button
+          <Percent
             title="10%"
             onPress={() => getTipPercent(10)}
           />
 
-          <Button
+          <Percent
             title="15%"
             onPress={() => getTipPercent(15)}
           />
 
-          <Button
+          <Percent
             title="20%"
             onPress={() => getTipPercent(20)}
           />
@@ -84,8 +84,9 @@ export default function Main() {
               paddingRight: 5,
             }}
           />
-        </View>
-        <Text>Quantas Pessoas irão pagar?</Text>
+        </PercentContainer>
+
+        <Paragraph>Quantas Pessoas irão pagar?</Paragraph>
         <CurrencyInput
           placeholder="Digite o valor"
           value={peopleNumber}
@@ -101,15 +102,18 @@ export default function Main() {
             paddingRight: 5,
           }}
         />
+
         <Button
           title="Calcular"
           onPress={tipSum}
         />
-        <Text>Valor total da gorjeta</Text>
-        <Text>{`R$ ${tipTotalValue}`}</Text>
-        <Text>Valor que cada pessoa ira pagar de gorjeta</Text>
-        <Text>{`R$ ${tipPerPeople}`}</Text>
-      </Container>
-    </View>
+      </Form>
+      <Div>
+        <Paragraph>Valor total da gorjeta</Paragraph>
+        <Paragraph>{`R$ ${tipTotalValue}`}</Paragraph>
+        <Paragraph>Valor que cada pessoa ira pagar de gorjeta</Paragraph>
+        <Paragraph>{`R$ ${tipPerPeople}`}</Paragraph>
+      </Div>
+    </Body>
   );
 }
--- gorjeta-app/src/screens/home/Style.js
+++ gorjeta-app/src/screens/home/Style.js
@@ -1,16 +1,51 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
+export const Body = styled.View`
+  flex: 1;
+  background-color: #E6F6F9;
+  width: 100%;
+`;
+
+export const Div = styled.View`
+  flex: 1;
+`;
+
+export const Header = styled.View`
+flex: 1;
+top: 100px;
+background-color: red;
+`;
+
+export const Paragraph = styled.Text``;
+
+export const Title = styled.Text`
+  margin: 1px;
+`;
+
+export const Form = styled.View`
+  flex: 1;
+  border: 1px solid red;
+  background-color: #FFFFFF;
+  padding: 15px;
+/*   height: 50%; */
+`;
+export const PercentContainer = styled.View`
   flex: 1;
   align-content: center;
   justify-content: center;
+  border: 1px solid blue;
 `;
 
-export const Title = styled.Text`
-  margin: 15px;
+export const Percent = styled.TouchableOpacity`
+  flex: 1;
+  border: 1px solid purple;
+  background-color: #FFFFFF;
+  height: 30px;
+  width: 100px;
 `;
 
 export const InputContainer = styled.View`
+  flex: 1;
   border-radius: 8px;
   align-items: center;
   align-content: center;
@@ -20,8 +55,6 @@ export const InputContainer = styled.View`
 `;
 
 export const Input = styled.View`
-  width: 80%;
-`;
-export const Symbol = styled.Text`
-
+  flex: 1;
+  width: 80px;
 `;
