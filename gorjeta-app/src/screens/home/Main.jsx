105d69fa0a690ef61972fbd1c7e4a6caab05717e
Gabriel <gabrielproencaaraujo@gmail.com>
2022-09-12 11:23:55 -0300

    Primeira versão finalizada

--- gorjeta-app/src/screens/home/Main.jsx
+++ gorjeta-app/src/screens/home/Main.jsx
@@ -2,168 +2,211 @@ import React, { useState } from 'react';
 import { Ionicons } from '@expo/vector-icons';
 import CurrencyInput from 'react-native-currency-input';
 import {
-  Title, InputContainer, Input, Header,
-  Body, PercentContainer, PercentButton, Form,
-  Symbol, Div, Container, Footer, TextBox,
+  Title, InputContainer, Input,
+  Body, PercentageContainer, PercentButton, Form,
+  Symbol, Div, Result, TextBox,
   CalculateButton, PercentText, CalculateText,
   ValueContainer, ValueText, TextBoxForm, SelectedPercentButton,
-  SelectedPercentText, Label,
+  SelectedPercentText, Label, ValueRequest, Warning, WarningInputContainer,
 } from './Style';
 
 export default function Main() {
-  const [requestValue, setRequestValue] = useState('');
-  const [tipPercent, getTipPercent] = useState('');
-  const [customTipPercent, getcustomTipPercent] = useState('');
-  const [peopleNumber, getPeopleNumber] = useState('');
-  const [tipTotalValue, getTipTotalValue] = useState('');
-  const [tipPerPeople, getTipPerPerson] = useState('');
-  const [fivePercent, getfivePercent] = useState(false);
-  const [tenPercent, getTenPercent] = useState(false);
-  const [fifteenPercent, getfifteenPercent] = useState(false);
-  const [Percent, gettwentyPercent] = useState(false);
+  const [orderValue, setOrderValue] = useState('');
+  const [tipPercent, setTipPercent] = useState('');
+  const [customTipPercent, setcustomTipPercent] = useState('');
+  const [peopleNumber, setPeopleNumber] = useState('');
+  const [tipTotalValue, setTipTotalValue] = useState('');
+  const [tipPerPeople, setTipPerPerson] = useState('');
+  const [fivePercent, setfivePercent] = useState(false);
+  const [tenPercent, setTenPercent] = useState(false);
+  const [fifteenPercent, setfifteenPercent] = useState(false);
+  const [twentyPercent, settwentyPercent] = useState(false);
+  const [ifOrderIsValid, setIfOrderIsValid] = useState(true);
+  const [ifPercentageIsValid, setIfPercentageIsValid] = useState(true);
+  const [ifNumberOfPeopleIsValid, setifNumberOfPeopleIsValid] = useState(true);
 
   const tipSum = () => {
-    const tipResult = (tipPercent / 100) * requestValue;
-    getTipTotalValue(tipResult.toFixed(2).replace('.', ','));
+    const tipResult = (tipPercent / 100) * orderValue;
+    setTipTotalValue(tipResult.toFixed(2).replace('.', ','));
     const tipPerPersonCalc = tipResult / peopleNumber;
-    getTipPerPerson(tipPerPersonCalc.toFixed(2).replace('.', ','));
+    setTipPerPerson(tipPerPersonCalc.toFixed(2).replace('.', ','));
+    chill();
   };
 
-  const customTipSum = () => {
-    const tipResult = (customTipPercent / 100) * requestValue;
-    getTipTotalValue(tipResult.toFixed(2).replace('.', ','));
-    const tipPerPersonCalc = tipResult / peopleNumber;
-    getTipPerPerson(tipPerPersonCalc.toFixed(2).replace('.', ','));
+  const setAllButtonFalse = () => {
+    setfivePercent(false);
+    setTenPercent(false);
+    setfifteenPercent(false);
+    settwentyPercent(false);
   };
 
-  const getAllButtonFlase = () => {
-    getfivePercent(false);
-    getTenPercent(false);
-    getfifteenPercent(false);
-    gettwentyPercent(false);
-  };
-
-  const handlePress = (percentValue) => {
-    getTipPercent(percentValue);
-    getcustomTipPercent(null);
-    getAllButtonFlase();
+  const percentPress = (percentValue) => {
+    setTipPercent(percentValue);
+    setcustomTipPercent(null);
+    setAllButtonFalse();
     switch (percentValue) {
       case 5:
-        getfivePercent(true);
+        setfivePercent(true);
         break;
       case 10:
-        getTenPercent(true);
+        setTenPercent(true);
         break;
       case 15:
-        getfifteenPercent(true);
+        setfifteenPercent(true);
         break;
       case 20:
-        gettwentyPercent(true);
+        settwentyPercent(true);
         break;
       default:
-        getfivePercent(false);
+        setAllButtonFalse();
     }
+  };
+
+  const customPercentage = (percentValue) => {
+    setTipPercent(percentValue);
+    setcustomTipPercent(percentValue);
+  };
+
+  const warning = () => {
+    if (orderValue === '' || orderValue === null) setIfOrderIsValid(false);
+    if (tipPercent === '' || tipPercent === null) setIfPercentageIsValid(false);
+    if (peopleNumber === '' || peopleNumber === null) setifNumberOfPeopleIsValid(false);
+  };
+
+  const chill = () => {
+    setIfOrderIsValid(true);
+    setIfPercentageIsValid(true);
+    setifNumberOfPeopleIsValid(true);
   };
 
   const calculatePress = () => {
-    if (customTipPercent > 0) {
-      return customTipSum();
+    const errors = [
+      orderValue > 0,
+      tipPercent > 0,
+      peopleNumber > 0,
+    ];
+    const ifIsValid = errors.every((error) => error === true);
+    switch (ifIsValid) {
+      case false:
+        warning();
+        break;
+      default:
+        tipSum();
     }
-    return tipSum();
   };
 
   return (
     <Body>
-      <Container>
-        <Header>
-          <Title>Gorjetas</Title>
-        </Header>
-        <Form>
+      <Title>Gorjetas</Title>
+      <Form>
+        <ValueRequest>
           <TextBoxForm>
             <Label>Digite o valor total do pedido</Label>
+            <Warning>*</Warning>
           </TextBoxForm>
-          <InputContainer>
-            <Symbol>R$</Symbol>
-            <Input>
-              <CurrencyInput
-                placeholder="Digite o valor aqui"
-                value={requestValue}
-                onChangeValue={setRequestValue}
-                prefix=""
-                delimiter="."
-                separator=","
-                precision={2}
-                style={{
-                  textAlign: 'right',
-                }}
-              />
-            </Input>
-          </InputContainer>
-          <TextBoxForm>
-            <Label>Quanto quer dar de gorjeta?</Label>
-          </TextBoxForm>
-          <PercentContainer>
-            <Div>
 
-              { fivePercent ? (
-                <SelectedPercentButton
-                  onPress={() => handlePress(5)}
-                >
-                  <SelectedPercentText>5%</SelectedPercentText>
-                </SelectedPercentButton>
-              ) : (
-                <PercentButton
-                  onPress={() => handlePress(5)}
-                >
-                  <PercentText>5%</PercentText>
-                </PercentButton>
-              ) }
+          { ifOrderIsValid ? (
+            <InputContainer>
+              <Symbol>R$</Symbol>
+              <Input>
+                <CurrencyInput
+                  placeholder="Digite o valor aqui"
+                  value={orderValue}
+                  onChangeValue={setOrderValue}
+                  prefix=""
+                  delimiter="."
+                  separator=","
+                  precision={2}
+                  style={{
+                    textAlign: 'right',
+                  }}
+                />
+              </Input>
+            </InputContainer>
+          ) : (
+            <WarningInputContainer>
+              <Symbol>R$</Symbol>
+              <Input>
+                <CurrencyInput
+                  placeholder="Digite o valor aqui"
+                  value={orderValue}
+                  onChangeValue={setOrderValue}
+                  prefix=""
+                  delimiter="."
+                  separator=","
+                  precision={2}
+                  style={{
+                    textAlign: 'right',
+                  }}
+                />
+              </Input>
+            </WarningInputContainer>
+          ) }
+        </ValueRequest>
+        <TextBoxForm>
+          <Label>Quanto quer dar de gorjeta?</Label>
+          <Warning>*</Warning>
+        </TextBoxForm>
+        <PercentageContainer>
+          <Div>
+            { fivePercent ? (
+              <SelectedPercentButton
+                onPress={() => percentPress(5)}
+              >
+                <SelectedPercentText>5%</SelectedPercentText>
+              </SelectedPercentButton>
+            ) : (
+              <PercentButton
+                onPress={() => percentPress(5)}
+              >
+                <PercentText>5%</PercentText>
+              </PercentButton>
+            ) }
+            { tenPercent ? (
+              <SelectedPercentButton
+                onPress={() => percentPress(10)}
+              >
+                <SelectedPercentText>10%</SelectedPercentText>
+              </SelectedPercentButton>
+            ) : (
+              <PercentButton
+                onPress={() => percentPress(10)}
+              >
+                <PercentText>10%</PercentText>
+              </PercentButton>
+            ) }
+          </Div>
+          <Div>
+            { fifteenPercent ? (
+              <SelectedPercentButton
+                onPress={() => percentPress(15)}
+              >
+                <SelectedPercentText>15%</SelectedPercentText>
+              </SelectedPercentButton>
+            ) : (
+              <PercentButton
+                onPress={() => percentPress(15)}
+              >
+                <PercentText>15%</PercentText>
+              </PercentButton>
+            )}
 
-              { tenPercent ? (
-                <SelectedPercentButton
-                  onPress={() => handlePress(10)}
-                >
-                  <SelectedPercentText>10%</SelectedPercentText>
-                </SelectedPercentButton>
-              ) : (
-                <PercentButton
-                  onPress={() => handlePress(10)}
-                >
-                  <PercentText>10%</PercentText>
-                </PercentButton>
-              ) }
-            </Div>
+            { twentyPercent ? (
+              <SelectedPercentButton
+                onPress={() => percentPress(20)}
+              >
+                <SelectedPercentText>20%</SelectedPercentText>
+              </SelectedPercentButton>
+            ) : (
+              <PercentButton
+                onPress={() => percentPress(20)}
+              >
+                <PercentText>20%</PercentText>
+              </PercentButton>
+            )}
+          </Div>
 
-            <Div>
-              { fifteenPercent ? (
-                <SelectedPercentButton
-                  onPress={() => handlePress(15)}
-                >
-                  <SelectedPercentText>15%</SelectedPercentText>
-                </SelectedPercentButton>
-              ) : (
-                <PercentButton
-                  onPress={() => handlePress(15)}
-                >
-                  <PercentText>15%</PercentText>
-                </PercentButton>
-              )}
-
-              { Percent ? (
-                <SelectedPercentButton
-                  onPress={() => handlePress(20)}
-                >
-                  <SelectedPercentText>20%</SelectedPercentText>
-                </SelectedPercentButton>
-              ) : (
-                <PercentButton
-                  onPress={() => handlePress(20)}
-                >
-                  <PercentText>20%</PercentText>
-                </PercentButton>
-              )}
-
-            </Div>
+          { ifPercentageIsValid ? (
             <InputContainer>
               <Symbol>%</Symbol>
               <Input>
@@ -171,8 +214,8 @@ export default function Main() {
                   maxValue={100}
                   placeholder="Personalize aqui"
                   value={customTipPercent}
-                  onChangeValue={getcustomTipPercent}
-                  onChange={getAllButtonFlase}
+                  onChangeValue={(value) => customPercentage(value)}
+                  onChange={setAllButtonFalse}
                   prefix=""
                   separator=","
                   precision={0}
@@ -181,17 +224,38 @@ export default function Main() {
                 />
               </Input>
             </InputContainer>
-          </PercentContainer>
-          <TextBoxForm>
-            <Label>Quantas Pessoas irão pagar?</Label>
-          </TextBoxForm>
+          ) : (
+            <WarningInputContainer>
+              <Symbol>%</Symbol>
+              <Input>
+                <CurrencyInput
+                  maxValue={100}
+                  placeholder="Personalize aqui"
+                  value={customTipPercent}
+                  onChangeValue={(value) => customPercentage(value)}
+                  onChange={setAllButtonFalse}
+                  prefix=""
+                  separator=","
+                  precision={0}
+                  suffix=""
+                  style={{ textAlign: 'right' }}
+                />
+              </Input>
+            </WarningInputContainer>
+          )}
+        </PercentageContainer>
+        <TextBoxForm>
+          <Label>Quantas Pessoas irão pagar?</Label>
+          <Warning>*</Warning>
+        </TextBoxForm>
+        { ifNumberOfPeopleIsValid ? (
           <InputContainer>
             <Ionicons name="person-outline" size={16} color="#01A7C2" />
             <Input>
               <CurrencyInput
                 placeholder="Digite o valor"
                 value={peopleNumber}
-                onChangeValue={getPeopleNumber}
+                onChangeValue={setPeopleNumber}
                 prefix=""
                 delimiter=""
                 separator=""
@@ -200,32 +264,45 @@ export default function Main() {
               />
             </Input>
           </InputContainer>
-          <CalculateButton
-            onPress={calculatePress}
-          >
-            <CalculateText>Calcular</CalculateText>
-          </CalculateButton>
-        </Form>
-        <Footer>
-
-          <TextBox>
-            <Label>Valor total da gorjeta</Label>
-          </TextBox>
-          <ValueContainer>
-            <ValueText>R$</ValueText>
-            <ValueText>{tipTotalValue}</ValueText>
-          </ValueContainer>
-
-          <TextBox>
-            <Label>Valor que cada pessoa ira pagar de gorjeta</Label>
-          </TextBox>
-          <ValueContainer>
-            <ValueText>R$</ValueText>
-            <ValueText>{tipPerPeople}</ValueText>
-          </ValueContainer>
-
-        </Footer>
-      </Container>
+        ) : (
+          <WarningInputContainer>
+            <Ionicons name="person-outline" size={16} color="#01A7C2" />
+            <Input>
+              <CurrencyInput
+                placeholder="Digite o valor"
+                value={peopleNumber}
+                onChangeValue={setPeopleNumber}
+                prefix=""
+                delimiter=""
+                separator=""
+                precision={0}
+                style={{ textAlign: 'right' }}
+              />
+            </Input>
+          </WarningInputContainer>
+        )}
+        <CalculateButton
+          onPress={calculatePress}
+        >
+          <CalculateText>Calcular</CalculateText>
+        </CalculateButton>
+      </Form>
+      <Result>
+        <TextBox>
+          <Label>Valor total da gorjeta</Label>
+        </TextBox>
+        <ValueContainer>
+          <ValueText>R$</ValueText>
+          <ValueText>{tipTotalValue}</ValueText>
+        </ValueContainer>
+        <TextBox>
+          <Label>Valor que cada pessoa ira pagar de gorjeta</Label>
+        </TextBox>
+        <ValueContainer>
+          <ValueText>R$</ValueText>
+          <ValueText>{tipPerPeople}</ValueText>
+        </ValueContainer>
+      </Result>
     </Body>
   );
 }
--- gorjeta-app/src/screens/home/Style.js
+++ gorjeta-app/src/screens/home/Style.js
@@ -1,40 +1,58 @@
 import styled from 'styled-components/native';
 
 export const Body = styled.View`
-margin-top: 25px;
-  font-family: Mulish-Regular;
-  width: 100%;
-`;
-
-export const Container = styled.View`
-  display: flex;
+display: flex;
   background-color: #F2F6FF;
   flex-direction: column;
   align-items: center;
-
-`;
-
-export const Header = styled.View`
-  display: flex;
-  justify-content: center;
-  height: 90px;
-  margin-top: 28px;
+  height: 100%;
+  width: 100%;
 `;
 
 export const Title = styled.Text`
   font-size: 24px;
   text-align: center;
   font-family: Mulish-Bold;
+  margin-top: 35px;
+  margin-bottom: 32px;
 `;
 
 export const Form = styled.View`
   display: flex;
   align-items: center;
-  border-radius: 8px;
+  border-radius: 24px;
   background-color: #FFFFFF;
   height: 487px;
   width: 327px;
   padding: 16px;
+  margin-bottom: 24px;
+`;
+
+export const ValueRequest = styled.View`
+  margin-bottom: 32px;
+`;
+
+export const TextBoxForm = styled.View`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  width: 295px;
+  margin-bottom: 8px;
+`;
+
+export const TextBox = styled.View`
+  width: 327px;
+  margin-bottom: 8px;
+`;
+export const Label = styled.Text`
+  font-family: Mulish-Regular;
+  font-size: 14px;
+`;
+
+export const Warning = styled.Text`
+  font-family: Mulish-Bold;
+  font-size: 14px;
+  color: #DF4A4A;
 `;
 
 export const InputContainer = styled.View`
@@ -42,54 +60,56 @@ export const InputContainer = styled.View`
   align-items: center;
   align-content: center;
   justify-content: space-between;
-  padding-left: 10px;
-  padding-right: 10px;
+  padding-left: 16px;
+  padding-right: 16px;
+  padding-top: 12px;
+  padding-bottom: 12px;
   flex-direction: row;
   background-color: #E6F6F9 ;
+  margin-bottom: 8px;
   width: 295px;
   height: 42px;
 `;
 
-export const InvalidInputContainer = styled.View`
+export const WarningInputContainer = styled.View`
   border-radius: 8px;
   align-items: center;
   align-content: center;
   justify-content: space-between;
-  padding-left: 10px;
-  padding-right: 10px;
+  padding-left: 16px;
+  padding-right: 16px;
+  padding-top: 12px;
+  padding-bottom: 12px;
   flex-direction: row;
   background-color: #E6F6F9 ;
   border: 1px solid #DF4A4A;
+  margin-bottom: 8px;
   width: 295px;
   height: 42px;
+`;
+
+export const Symbol = styled.Text`
+  font-family: Mulish-Bold;
+  font-size: 14px;
 `;
 
 export const Input = styled.View`
   width: 80%;
 `;
 
-export const PercentContainer = styled.View`
+export const Div = styled.View`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+`;
+
+export const PercentageContainer = styled.View`
   display: flex;
   flex-direction: column;
   align-content: center;
   justify-content: center;
-`;
-
-export const PercentButton = styled.TouchableOpacity`
-  justify-content: center;
-  align-items: center;
-  border: 1px solid #01A7C2;
-  background-color: #FFFFFF;
-  height: 42px;
-  width: 143px;
-  margin: 9px;
-  border-radius: 10px;
-`;
-
-export const PercentText = styled.Text`
-  color: #01A7C2;
-  font-family: Mulish-Regular;
-  font-size: 14px;
+  margin-bottom: 8px;
 `;
 
 export const SelectedPercentButton = styled.TouchableOpacity`
@@ -99,12 +119,31 @@ export const SelectedPercentButton = styled.TouchableOpacity`
   background-color: #0E7182;
   height: 42px;
   width: 143px;
-  margin: 9px;
+  margin-right: 9px;
+  margin-bottom: 8px;
   border-radius: 10px;
 `;
 
 export const SelectedPercentText = styled.Text`
   color: #FFFFFF;
+  font-family: Mulish-Regular;
+  font-size: 14px;
+`;
+
+export const PercentButton = styled.TouchableOpacity`
+  justify-content: center;
+  align-items: center;
+  border: 1px solid #01A7C2;
+  background-color: #FFFFFF;
+  height: 42px;
+  width: 143px;
+  margin-right: 9px;
+  margin-bottom: 8px;
+  border-radius: 10px;
+`;
+
+export const PercentText = styled.Text`
+  color: #01A7C2;
   font-family: Mulish-Regular;
   font-size: 14px;
 `;
@@ -117,8 +156,7 @@ export const CalculateButton = styled.TouchableOpacity`
   height: 42px;
   width: 160px;
   border-radius: 10px;
-  margin: 37px;
-  margin-bottom: 32px;
+  margin-top: 29px;
 `;
 
 export const CalculateText = styled.Text`
@@ -127,14 +165,13 @@ export const CalculateText = styled.Text`
   font-size: 16px;
 `;
 
-export const Footer = styled.View`
+export const Result = styled.View`
   display: flex;
   width: 100%;
   justify-content: center;
   align-items: center;
   border-radius: 10px;
   padding: 10px;
-  margin-bottom: 32px;
 `;
 
 export const ValueContainer = styled.View`
@@ -143,6 +180,7 @@ export const ValueContainer = styled.View`
   align-content: center;
   width: 327px;
   height: 42px;
+  margin-bottom: 24px;
   justify-content: space-between;
   padding-left: 10px;
   padding-right: 10px;
@@ -154,32 +192,4 @@ export const ValueText = styled.Text`
   color: #FFFFFF;
   font-family: Mulish-Bold;
   font-size: 14px;
-`;
-
-export const Symbol = styled.Text`
-  margin: 5px;
-  font-family: Mulish-Bold;
-  font-size: 14px;
-`;
-
-export const Label = styled.Text`
-  margin: 5px;
-  font-family: Mulish-Regular;
-  font-size: 14px;
-`;
-export const Div = styled.View`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-around;
-`;
-
-export const TextBoxForm = styled.View`
-width: 295px;;
-margin: 5px;
-`;
-
-export const TextBox = styled.View`
-width: 327px;;
-margin-top: 24px;
 `;
